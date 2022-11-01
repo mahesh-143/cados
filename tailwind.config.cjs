36f0d5931634596ccd8f9817b251216b12a1e78a
mahesh-143 <maheshodedara13@yahoo.com>
2022-11-01 20:18:41 +0530

    added montserrat fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors : {
-      white : "#FFFFFF",
-      black : "#000000",
-      accent : "#D1EE6B",
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      accent: "#D1EE6B",
       "bg-light": "#FAF9EF",
-      "bg-dark" : "#1C1C1C",
-      "card-dark" : "1F1F1F",
+      "bg-dark": " #1C1C1C",
+      "card-dark": "#1F1F1F",
     },
-    fontFamily : {
-      poppins : "Poppins, sans-serif",
+    fontFamily: {
+      poppins: "Poppins, sans-serif",
+      montserrat: "Montserrat, sans-serif",
     },
     extend: {},
   },
