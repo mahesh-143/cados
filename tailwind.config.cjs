a5bb60be7ff66475e63ce44be199b1cc16e0f6d0
mahesh-143 <maheshodedara13@yahoo.com>
2022-11-01 17:15:33 +0530

    customized design system

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors : {
+      white : "#FFFFFF",
+      black : "#000000",
+      accent : "#D1EE6B",
+      "bg-light": "#FAF9EF",
+      "bg-dark" : "#1C1C1C",
+      "card-dark" : "1F1F1F",
+    },
+    fontFamily : {
+      poppins : "Poppins, sans-serif",
+    },
     extend: {},
   },
   plugins: [],
