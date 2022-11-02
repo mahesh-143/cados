a7dd670f5c086db0fbd373a7de3386ffa870d945
mahesh-143 <maheshodedara13@yahoo.com>
2022-11-02 20:09:43 +0530

    added hero text

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import Hero from "./components/Hero"
 
 function App() {
   return (
     <div className=" h-screen bg-bg-light dark:bg-bg-dark ">
       <Header />
+      <Hero />
     </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Hero = () => {
+
+  return (
+    <div className=" max-w-screen-lg m-auto flex flex-col items-center gap-6 mt-16 px-4">
+    <h1 className=" font-medium text-4xl md:text-5xl max-w-[46.25rem] md:leading-[4.5rem] text-center dark:text-white">Meet & Collab With <span className="bg-accent text-black">Advocate</span> from Different Companies</h1>
+    <p className=" text-lg max-w-md text-center text-black/50 dark:text-white/50">Search 2112 developer advocates found by @dennisivy's webscraper and the TwitterAPI.</p>
+    </div>
+  )
+}
+
+export default Hero
