58c441abea80378511c0dc991cd96146469d6379
mahesh-143 <maheshodedara13@yahoo.com>
2022-11-03 17:43:14 +0530

    adjusted margin

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 import React from "react"
 
-const Hero = () => {
+const Hero = (props) => {
 
   return (
-    <div className=" max-w-screen-lg m-auto flex flex-col items-center gap-6 mt-16 px-4">
+    <div className=" max-w-screen-lg m-auto flex flex-col items-center gap-6 mt-20 px-4">
     <h1 className=" font-medium text-4xl md:text-5xl max-w-[46.25rem] md:leading-[4.5rem] text-center dark:text-white">Meet & Collab With <span className="bg-accent text-black">Advocate</span> from Different Companies</h1>
-    <p className=" text-lg max-w-md text-center text-black/50 dark:text-white/50">Search 2112 developer advocates found by @dennisivy's webscraper and the TwitterAPI.</p>
+    <p className=" text-lg max-w-md text-center text-black/50 dark:text-white/50">{props.totalResults} developer advocates found by @dennisivy's webscraper and the TwitterAPI.</p>
     </div>
   )
 }
