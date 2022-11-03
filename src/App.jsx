b2661b3b8cb95180ad81ba1194a1e31f7318cc34
mahesh-143 <maheshodedara13@yahoo.com>
2022-11-03 18:54:25 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Header from "./components/Header"
 import Home from "./pages/Home"
+import Advocate from "./pages/Advocate"
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/advocate/:username" element={<Advocate />} />
+      </Routes>
+      
     </div>
   )
 }
