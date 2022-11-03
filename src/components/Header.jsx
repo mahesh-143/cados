405cfae003a06f13ae77308720a031ab0c404531
mahesh-143 <maheshodedara13@yahoo.com>
2022-11-03 19:00:47 +0530

    added logo in its own component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-import ThemeToggle from "./ThemeToggle"
+import Logo from "./Logo"
 import Searchbox from "./Searchbox"
+import ThemeToggle from "./ThemeToggle"
+
 const Header = () => {
   return (
     <header className=" bg-white/50 text-black dark:bg-black/50 dark:text-white backdrop-blur-[10px] h-16 w-full  sticky top-0 z-50">
       <div className="flex items-center justify-between max-w-7xl m-auto px-4 h-full">
-      <h1 className=" font-bold text-2xl md:text-3xl font-montserrat ">Cados.</h1>
+      <Logo />
       <Searchbox />
       <ThemeToggle />
       </div>
---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+
+const Logo = () => {
+  return (
+    <Link to="/"><h1 className=" font-bold text-2xl md:text-3xl font-montserrat ">Cados.</h1></Link>
+  )
+}
+
+export default Logo
