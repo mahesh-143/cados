888d353193a29bf3568deb844975f09f7ef9d699
mahesh-143 <maheshodedara13@yahoo.com>
2022-11-03 17:44:30 +0530

    added Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom"
+
+const Card = (props) => {
+  return (
+    <div className="w-96 bg-white dark:bg-card-dark px-5 py-6 rounded-[10px] m-4">
+      <Link to={`/advocate/${props.username}`}>
+        <div className="flex gap-6 items-center mb-4">
+          <img
+            src={props.profile_pic}
+            alt="Profile Picture"
+            className="w-14 h-14 border border-accent rounded-full "
+          />
+          <div>
+            <h3 className="font-medium dark:text-white">{props.name}</h3>
+            <span className="text-black/50 dark:text-white/50 text-sm">
+              @{props.username}
+            </span>
+          </div>
+        </div>
+      </Link>
+
+      <p className="text-sm dark:text-white">{props.bio}</p>
+    </div>
+  )
+}
+
+export default Card
