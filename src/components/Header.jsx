d1552b97683a1b3450a8fa5355befab07bec6352
mahesh-143 <maheshodedara13@yahoo.com>
2022-11-03 17:53:38 +0530

    made header stiky and added container margin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,12 @@ import ThemeToggle from "./ThemeToggle"
 import Searchbox from "./Searchbox"
 const Header = () => {
   return (
-    <header className="flex items-center justify-between bg-white/50 text-black dark:bg-black/50 dark:text-white backdrop-blur-[10px] h-16 w-full px-4">
+    <header className=" bg-white/50 text-black dark:bg-black/50 dark:text-white backdrop-blur-[10px] h-16 w-full  sticky top-0 z-50">
+      <div className="flex items-center justify-between max-w-7xl m-auto px-4 h-full">
       <h1 className=" font-bold text-2xl md:text-3xl font-montserrat ">Cados.</h1>
       <Searchbox />
       <ThemeToggle />
+      </div>
     </header>
   )
 }
