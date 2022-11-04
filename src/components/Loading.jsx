6080bb8790737d12273647249ae5dd14991cce45
mahesh-143 <maheshodedara13@yahoo.com>
2022-11-04 18:37:52 +0530

    added styles for darkmode in loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
 
 const Loading = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center">Loading...</div>
+    <div className="w-full h-screen flex items-center justify-center dark:text-white">Loading...</div>
   )
 }
 
