da995eb983099fc5e081b2fa48d0e07ab3d68f5e
mahesh-143 <maheshodedara13@yahoo.com>
2022-11-04 20:10:30 +0530

    adjusted line height on smaller devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = (props) => {
 
   return (
     <div className=" max-w-screen-lg m-auto flex flex-col items-center gap-6 mt-20 px-4">
-    <h1 className=" font-medium text-4xl md:text-5xl max-w-[46.25rem] md:leading-[4.5rem] text-center dark:text-white">Meet & Collab With <span className="bg-accent text-black">Advocate</span> from Different Companies</h1>
+    <h1 className=" font-medium text-4xl md:text-5xl max-w-[46.25rem] leading-[3rem] md:leading-[4.5rem] text-center dark:text-white">Meet & Collab With <span className="bg-accent text-black">Advocate</span> from Different Companies</h1>
     <p className=" text-lg max-w-md text-center text-black/50 dark:text-white/50">{props.totalResults} developer advocates found by @dennisivy's webscraper and the TwitterAPI.</p>
     </div>
   )
