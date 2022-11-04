1fa41e45180d7c886f454287d12397b964eae571
mahesh-143 <maheshodedara13@yahoo.com>
2022-11-04 10:43:13 +0530

    added margin top to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Logo from "./Logo"
 
 const Footer = () => {
   return (
-    <div className="w-full h-24 bg-white/50 dark:bg-black/50 dark:text-white backdrop-blur-[10px] ">
+    <div className="w-full h-24 bg-white/50 dark:bg-black/50 dark:text-white backdrop-blur-[10px] mt-10 ">
       <footer className="flex justify-between items-center gap-4 h-full max-w-7xl m-auto px-4">
         <Logo />
         <p className="text-black/50 dark:text-white/50">© 2022 Cados.dev, all rights reserved.</p>
